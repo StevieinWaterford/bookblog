117c4949da62dbaea6338e29dc1d88db587dede4
StevieinWaterford <stevieindub@gmail.com>
2021-12-08 14:12:47 +0000

    Add deployment of files

---
+++ env.py
@@ -0,0 +1,5 @@
+import os 
+
+os.environ["DATABASE_URL"] = "postgres://hinqhkbzboqewm:e1f18f60eba7760d8744a3754f6bf982151ea075cfa216999fb75b6f2efa2dc8@ec2-54-76-249-45.eu-west-1.compute.amazonaws.com:5432/d4b253j8o8vob5"
+os.environ["SECRET_KEY"] = "randomSecretKey2022!!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://884173494267683:2A1aK79n30uxfpSjxtgcjAXpt_g@dzbad1rgb"
--- mybookblog/settings.py
+++ mybookblog/settings.py
@@ -11,21 +11,25 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_95-w=5z3-!8h*50d*-mhkyxkd02$g9zrh121!0kg4cl=2zb=5'
-
+SECRET_KEY = os.environ.get ('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mybookblog2021.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -36,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'blog',
 ]
 
@@ -55,7 +61,7 @@ ROOT_URLCONF = 'mybookblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,11 +80,16 @@ WSGI_APPLICATION = 'mybookblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
 }
 
 
@@ -117,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL  = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
